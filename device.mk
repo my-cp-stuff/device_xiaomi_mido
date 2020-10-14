b153c35ba320a8aaa256f674f81366ac8a9d056a
Lucchetto <lucchetto.tie@live.com>
2020-10-14 17:16:56 +0200

    mido: build libhidltransport
        * Required by some blobs

--- device.mk
+++ device.mk
@@ -210,9 +210,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     libhidltransport \
-    libhidltransport.vendor \
-    libhwbinder \
-    libhwbinder.vendor
+    libhidltransport.vendor
 
 # HW crypto
 PRODUCT_PACKAGES += \
