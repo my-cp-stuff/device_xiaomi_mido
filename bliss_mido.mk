b9893522ae37d022420c5bf988b13e55c9446edd
Erickeagle3 <sayanchakraborty334@gmail.com>
2020-12-10 03:10:55 +0000

    u

--- bliss_mido.mk
+++ bliss_mido.mk
@@ -28,7 +28,7 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Bliss build type
-BLISS_BUILDTYPE=OFFICIAL
+BLISS_BUILDTYPE=UNOFFICIAL
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
